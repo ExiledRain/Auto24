b0fcf0d15c1812aca4836bb1e490edc8b4faba1a
exiled <gett_lostt@bk.ru>
2022-12-18 23:07:40 +0200

    Initial commit

---
+++ src/main/java/io/exiled/auto24tg/Auto24tgApplication.java
@@ -0,0 +1,13 @@
+package io.exiled.auto24tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auto24tgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auto24tgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/exiled/auto24tg/Auto24tgApplicationTests.java
@@ -0,0 +1,13 @@
+package io.exiled.auto24tg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auto24tgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
